14e622c2a3f029ab0c81a6e20d1d423da9a20c9c
AlexandroMtzG <alex.martinez@absys.com.mx>
2023-06-06 11:20:49 -0600

    remix version to 1.14.3

--- remix.config.js
+++ remix.config.js
@@ -2,11 +2,12 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  serverBuildTarget: "vercel",
+  serverDependenciesToBundle: ["marked"],
+  // serverBuildTarget: "vercel",
   // When running locally in development mode, we use the built in remix
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  // server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: [".*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
